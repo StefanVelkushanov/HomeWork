36671be09d55ac0983ef603addc7c8f5ea59d0a4
StefanVelkushanov <61464226+StefanVelkushanov@users.noreply.github.com>
2020-02-28 15:18:22 +0200

    v3
    v3

--- Lecutre2HomeWork/Start.cs
+++ Lecutre2HomeWork/Start.cs
@@ -1,4 +1,4 @@
-﻿using Exercise2;
+﻿using Lecture2HomeWork;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,18 +9,23 @@ namespace Lecutre2HomeWork
     {
         public static void Main(string[] args)
         {
-            // Calculations01.Main5();
-            // Calculations02.Main6();
-            // Calculations03.Main7();
-            // Calculations04.Main8();
-            Calculations05.Main9();
-            Calculations06.Main10();
-            Calculations07.Main11();
-            Calculations08.Main12();
-            Calculations09.Main13();
-            Calculations10.Main14();
-            Calculations11.Main15();
-
+            //Calculations01.Main5();
+            //Calculations02.Main6();
+            //Calculations03.Main7();
+            //Calculations04.Main8();
+            //Calculations05.Main9();
+            //Calculations06.Main10();
+            //Calculations07.Main11();
+            //Calculations08.Main12();
+            //Calculations09.Main13();
+            //Calculations10.Main14();
+            //Calculations11.Main15();
+            //Conditions01.Main16();
+            //Conditions02.Main17();
+            //Conditions03.Main18();
+            //Conditions04.Main19();
+            //Conditions05.Main20();
+            //Conditions06.Main21();
         }
     }
 }
